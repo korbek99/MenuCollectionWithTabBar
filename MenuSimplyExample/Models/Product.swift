73b66b0bcc1987ec1159ffe5fa2af5e65d45b56b
JosePreatorianWise <jose.david.bustos.h@gmail.com>
2024-09-28 23:24:32 -0300

    first Commit

---
+++ MenuSimplyExample/Models/Product.swift
@@ -0,0 +1,40 @@
+//
+//  Product.swift
+//  MenuSimplyExample
+//
+//  Created by Jose David Bustos H on 28-09-17.
+//
+
+import Foundation
+
+struct Product {
+    let itemName: String
+    let photo: String
+    let description: String
+    let typeDevice: Int
+}
+
+
+let productList: [Product] = [
+
+    Product(itemName: "MacBook air i3", photo: "https://teamueblastore.cl/wp-content/uploads/2021/10/Macbook-Air-13%E2%80%B3-Intel-Core-I3-8GB-RAM-256GB-SSD-Gold.jpg", description: "MacBook Mini i5 4GB RAM 256 SDD 13 $599.990", typeDevice: 1),
+    Product(itemName: "MacBook Air i5", photo: "https://happybytes.nl/wp-content/uploads/2020/05/Apple_MacBook_Air_13inch_A1932_Big_Sur_1_1200x796-1024x679.jpg", description: "MacBook Air Intel Core i5 8GB RAM 128GB SDD 13 $549.990", typeDevice: 1),
+    Product(itemName: "MacBook Retina i7", photo: "https://thangpro.vn/uploads/source/laptop/macbook/pro-2016-15/img-8303.jpg", description: "MacBook Air Intel Core i5 8GB RAM 256GB SDD 13 $899.990", typeDevice: 1),
+    Product(itemName: "MacBook Pro i5", photo: "https://www.affordablemac.co.uk/wp-content/uploads/2022/10/Macbook-Pro-13-Space-Grey-1.png", description: "MacBook Air Intel Core i5 8GB RAM 512GB SDD 13 $499.990", typeDevice: 1),
+    Product(itemName: "MacBook 11 i5", photo: "https://3.bp.blogspot.com/-KwRaW130wN4/WseBDT1VQUI/AAAAAAAATyM/3Czi_3ZLLUgrAqBflAn6i71YWuMbhMsTwCLcBGAs/s1600/maccc.jpg", description: "MacBook Air Intel Core i5 8GB RAM 1T SDD 11 $599.990", typeDevice: 1),
+    Product(itemName: "MacBook air i3", photo: "5771915", description: "MacBook Air Intel Core i5 8GB RAM 1T SDD 13 $699.990", typeDevice: 1),
+    Product(itemName: "Funda Plastica Macbook", photo: "https://images-na.ssl-images-amazon.com/images/I/71fZVPNmIHL._AC_SL1500_.jpg", description: "Funda Plastica Macbook $7.990", typeDevice: 2),
+    Product(itemName: "Conexion HDMI", photo: "https://images-na.ssl-images-amazon.com/images/I/71bBlSkD9OL._AC_SL1500_.jpg", description: "Conexion HDMI $8.990", typeDevice: 2),
+    Product(itemName: "Funda con Cierre", photo: "https://http2.mlstatic.com/D_NQ_NP_995656-MCO51184374236_082022-O.jpg", description: "Funda con Cierre tipo Bolso $9.990", typeDevice: 2),
+    Product(itemName: "Mouse Mac", photo: "https://dev.mos.cms.futurecdn.net/vD9XBcCWnmKRbyiB82bvwW.jpg", description: "Mouse Mac $12.990", typeDevice: 2)
+]
+
+
+let productListDevices: [Product] = [
+
+    Product(itemName: "Funda Plastica Macbook", photo: "5678940", description: "Funda Plastica Macbook $7.990", typeDevice: 2),
+    Product(itemName: "Conexion HDMI", photo: "5678941", description: "Conexion HDMI $8.990", typeDevice: 2),
+    Product(itemName: "Funda con Cierre", photo: "5678942", description: "Funda con Cierre tipo Bolso $9.990", typeDevice: 2),
+    Product(itemName: "Mouse Mac", photo: "5678945", description: "Mouse Mac $12.990", typeDevice: 2)
+]
+
